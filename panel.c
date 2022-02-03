442a17b51466f8d89a5eea090a1453b1cd4a80b2
Pawel Piecuch <piecuch.pawel@gmail.com>
2022-02-03 19:28:37 +0000

    more ui work

--- panel.c
+++ panel.c
@@ -1,6 +1,7 @@
 #include "lvgl/lvgl.h"
 #include "lvgl/lv_drivers/display/fbdev.h"
 #include "lvgl/lv_drivers/indev/evdev.h"
+#include <time.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -14,6 +15,58 @@ static lv_disp_buf_t disp_buf;
 static lv_color_t lvbuf1[LV_BUF_SIZE];
 static lv_color_t lvbuf2[LV_BUF_SIZE];
 
+// Display info and controls
+unsigned long int kb_sent, kb_sent_prev = 0;
+const char* DAY[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const char* MONTH[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+char timeString[9];
+char dateString[16];
+
+static lv_obj_t *clock_label;
+static lv_obj_t *date_label;
+
+
+// Utilities functions
+
+static void time_timer_cb(lv_task_t *timer) {
+
+    time_t t = time(NULL);
+    struct tm *local = localtime(&t);
+
+    sprintf(timeString, "%02d:%02d:%02d", local->tm_hour, local->tm_min, local->tm_sec);
+    sprintf(dateString, "%s\n%s %02d %04d", DAY[local->tm_wday], MONTH[local->tm_mon], local->tm_mday, local->tm_year + 1900);
+
+    lv_label_set_text(clock_label, timeString);
+    lv_label_set_text(date_label, dateString);
+}
+
+static int get_current_network_speed() {
+    FILE *fp = fopen("/proc/net/dev", "r");
+    char buf[200], ifname[20];
+    unsigned long int r_bytes, t_bytes, r_packets, t_packets;
+
+    // skip first two lines
+    for (int i = 0; i < 2; i++) {
+        fgets(buf, 200, fp);
+    }
+
+    while (fgets(buf, 200, fp)) {
+        sscanf(buf, "%[^:]: %lu %lu %*lu %*lu %*lu %*lu %*lu %*lu %lu %lu", ifname, &r_bytes, &r_packets, &t_bytes, &t_packets);
+        if(strstr(ifname, "eth0") != NULL) {
+	  kb_sent = r_bytes / 1024;
+        }
+    }
+
+    unsigned long int eth0_speed = (kb_sent - kb_sent_prev) * 2;
+    kb_sent_prev = kb_sent;
+
+    fclose(fp);
+    return eth0_speed;
+}
+
+
+//  Main loop
 
 int main(void)
 {
