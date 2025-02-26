a6bf61d3eed4d51e54ed0ea0208af367e5e7bc43
Pawel Piecuch <piecuch.pawel@gmail.com>
2025-02-26 21:25:14 +0100

    cleanup; update lvgl

--- lvgl-7.0.0/lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.mk
+++
@@ -1,6 +0,0 @@
-CSRCS += lv_demo_benchmark.c
-
-DEPPATH += --dep-path $(LVGL_DIR)/lv_examples/src/lv_demo_benchmark
-VPATH += :$(LVGL_DIR)/lv_examples/src/lv_demo_benchmark
-CFLAGS += "-I$(LVGL_DIR)/lv_examples/src/lv_demo_benchmark"
-
--- lvgl-7.0.0/lv_examples/src/lv_demo_stress/lv_demo_stress.mk
+++
@@ -1,6 +0,0 @@
-CSRCS += lv_demo_stress.c
-
-DEPPATH += --dep-path $(LVGL_DIR)/lv_examples/src/lv_demo_stress
-VPATH += :$(LVGL_DIR)/lv_examples/src/lv_demo_stress
-CFLAGS += "-I$(LVGL_DIR)/lv_examples/src/lv_demo_stress"
-
