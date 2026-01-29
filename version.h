b9a985ae3b5b289cda5ae1071ad0bbc24a0b4255
Pawel Piecuch <piecuch.pawel@gmail.com>
2026-01-29 16:27:11 +0100

    improve build process

---
+++ version.h
@@ -0,0 +1,12 @@
+#ifndef PANEL_VERSION_H
+#define PANEL_VERSION_H
+#ifndef PANEL_VERSION
+#define PANEL_VERSION "0.1.0"
+#endif
+#ifndef PANEL_BUILD_DATE
+#define PANEL_BUILD_DATE __DATE__
+#endif
+#ifndef PANEL_BUILD_HASH
+#define PANEL_BUILD_HASH "unknown"
+#endif
+#endif
